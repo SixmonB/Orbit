218fc56272d65aa5e72600de96ea45381385ddfd
Simon Bruno <s1bruno@enib.fr>
2023-03-06 23:44:07 +0100

    Added event handler function to change color to cube and sphere when clicking on the cube. For now, clickin on the sphere doesn't do anything

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class WorldGenerator : MonoBehaviour
+public class WorldGenerator : MonoBehaviour, UnityEngine.EventSystems.IPointerClickHandler
 {
     private GameObject sphere_instance;
     private GameObject cube_instance;
     public GameObject orbit_object;
     public GameObject central_object;
     public float orbitRadius;
+    private MeshRenderer meshRenderer;
     public float orbitSpeed;
     private Vector3 orbitAxis;
     // Start is called before the first frame update
@@ -16,7 +18,7 @@ public class WorldGenerator : MonoBehaviour
     {
         orbitSpeed = 50.0f;
         orbitRadius = 2.0f;
-        cube_instance = Instantiate(central_object);
+        cube_instance = Instantiate(central_object, transform);
         sphere_instance = Instantiate(orbit_object);
         sphere_instance.transform.position = new Vector3(orbitRadius, 0.0f, orbitRadius);
         cube_instance.transform.position = new Vector3(0f, 0f, 0f);
@@ -27,5 +29,15 @@ public class WorldGenerator : MonoBehaviour
     void Update()
     {
         sphere_instance.transform.RotateAround(cube_instance.transform.position, orbitAxis, orbitSpeed * Time.deltaTime);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        meshRenderer = sphere_instance.GetComponent<MeshRenderer>();
+        meshRenderer.material.color = new Color(Random.value, Random.value, Random.value);
+        
+        meshRenderer = cube_instance.GetComponent<MeshRenderer>();
+        meshRenderer.material.color = new Color(Random.value, Random.value, Random.value);
+        // Do something when the cube instance is clicked
     }
 }
